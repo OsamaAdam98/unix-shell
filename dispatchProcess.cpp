aefcd5461a366edc3fe9442889fe436ad80f66ab
Osama Adam <osamaadam98@gmail.com>
2020-03-27 22:17:15 +0200

    background launch working but not as a child??

--- dispatchProcess.cpp
+++ dispatchProcess.cpp
@@ -14,24 +14,33 @@ void dispatchProcess(int argc, char* argv[]) {
   //   cout << argv[i] << endl;
   // }
 
-  pid processID = fork();
-  if (processID < 0) {
-    perror("Fork failed!\n");
-    exit(1);
-  } else if (processID == 0 && argc > 1) {
-    char* args[argc];
-    for (int i = 0; i <= argc; i++) {
-      if (i == argc)
-        args[i] = NULL;
-      else
-        args[i] = (char*)argv[i];
-    }
-    if (!strcmp(args[0], "cd"))
-      if (!args[1] || !strcmp(args[1], "~"))
-        chdir(getenv("HOME"));
-      else
-        chdir(args[1]);
+  char* args[argc];
+  for (int i = 0; i <= argc; i++) {
+    if (i == argc)
+      args[i] = NULL;
     else
-      execvp(args[0], args);
+      args[i] = (char*)argv[i];
+  }
+  if (argc > 1) {
+    pid processID = fork();
+
+    if (processID < 0) {
+      perror("Fork failed!\n");
+      exit(1);
+    } else if (processID == 0) {
+      if (!strcmp(args[argc - 2], "&")) {
+        args[argc - 2] = NULL;
+        dispatchProcess(argc - 1, args);
+      } else {
+        if (!strcmp(args[0], "cd")) {
+          if (!args[1] || !strcmp(args[1], "~"))
+            chdir(getenv("HOME"));
+          else
+            chdir(args[1]);
+        } else
+          execvp(args[0], args);
+      }
+      exit(0);
+    }
   }
 }
