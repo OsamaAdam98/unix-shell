7ae5a566db30dd5defa09137793f24ac6a68dcff
Osama Adam <osamaadam98@gmail.com>
2020-03-30 04:21:47 +0200

    truncating current working directory

--- prompt.hpp
+++ prompt.hpp
@@ -1,12 +1,16 @@
 #ifndef PROMPT
 #define PROMPT
 #include <iostream>
-
 using namespace std;
 
 string prompt(char *username, char *cwd) {
   string userInput;
-  cout << username << "@shell " << cwd << "=> ";
+  string cwdString = string(cwd);
+
+  if (cwdString.find("home") < cwdString.length())
+    cwdString.replace(0, 6 + strlen(username), "~");
+
+  cout << username << "@shell " << cwdString << " => ";
   getline(cin, userInput);
   if (cin.eof()) return "logout";
   return userInput;
